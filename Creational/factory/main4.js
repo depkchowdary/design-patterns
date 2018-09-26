29a4435c3295472c6225b5240e6273fc0be07146
Tatineni <Deepak.Tatineni@mastercard.com>
2018-09-27 01:53:32 +0530

    Factory pattern working

--- Creational/factory/main4.js
+++ Creational/factory/main4.js
@@ -1,13 +1,18 @@
+/* A simplified repository factory object creation pattern using cache and dot notation pattern */
+
 
 var repo = require('./repoFactory2')
 var Task = require('./task')
 var User = require('./user')
 var Project = require('./project')
 
+
+console.log(repo.task.get(1))
+
 var authetication = new Task(repo.task.get(1))
 
 var user = new User(repo.user.get(2))
-var project = new Project(repo.project.get(ddjsjfhdkklncvb))
+var project = new Project(repo.project.get("Matrix"))
 
 authetication.addUser(user)
 project.addTask(authetication)
--- Creational/factory/repoFactory2.js
+++ Creational/factory/repoFactory2.js
@@ -3,11 +3,12 @@
 
 var repoFactory = function(){
         var repos = this;
-        var repoList = [{ name: "task", source: "./task"},
-                        {name: "user", source: "./user"},
-                        {name: "project", source: "./project"}]
+        console.lo
+        var repoList = [{ name: "task", source: "./taskRepository"},
+                        {name: "user", source: "./userRepository"},
+                        {name: "project", source: "./projectRepository"}]
         repoList.forEach((repo) => {
-            repos[repo.name] = require(repo.source)()
+            repos[repo.name] = require(repo.source)();
         })
     }
 
